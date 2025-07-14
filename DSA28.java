bd4ffb887e2d9187240c8da7585b01278df50fbe
Mr-Priyanshu-Gupta <priyanshuguptaofficial555@gmail.com>
2025-07-14 23:12:19 +0530

    Add files via upload

---
+++ DSA28.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class DSA28
+{
+	public static void main(String args[])
+	{
+	  Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your binary number");
+ 		 //int num1 = sc.nextInt();
+			String hexStr = sc.nextLine();
+	 	System.out.println(" hexadec number is :" + hexStr);
+			int decnum = Integer.parseInt(hexStr, 16);
+			
+			System.out.println(decnum); 
+			
+			
+			
+			
+			
+ }		
+
+ }
