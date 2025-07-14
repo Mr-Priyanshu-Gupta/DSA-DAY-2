0513631cc4def53d3b62625cdf5a427dbf735553
Mr-Priyanshu-Gupta <priyanshuguptaofficial555@gmail.com>
2025-07-14 17:38:25 +0530

    Add files via upload

---
+++ DSA23.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class DSA23
+{
+	public static void main(String args[])
+	{
+	  Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your binary number");
+ 		 int num1 = sc.nextInt();
+	 	System.out.println(" binary number is :" + num1);
+		int power = 1;
+		int result = 0 ;
+			while(num1 > 0 ){
+				int x = num1 % 10;
+				result = result + (x * power);
+				power = power * 2;
+				num1 = num1 / 10;
+			}
+//binary to hexa dec
+
+		 if(result == 0){
+		System.out.println("HEXAdec : 0");
+		 }
+		while(result > 0){
+		 int HEXAdec = result % 16;
+		 result = result /16;
+		System.out.println("HEXAdecimal value : " + HEXAdec);
+}
+ }
+}
