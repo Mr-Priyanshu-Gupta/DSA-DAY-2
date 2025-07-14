e21eb8c85dd563ea75af67b96e9b3eb3b65490ec
Mr-Priyanshu-Gupta <priyanshuguptaofficial555@gmail.com>
2025-07-14 23:30:13 +0530

    Add files via upload

---
+++ DSA29.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class DSA29
+{
+	public static void main(String args[])
+	{
+	  Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your Hexadec  number");
+ 		 //int num1 = sc.nextInt();
+			String hexStr = sc.nextLine();
+	 	System.out.println(" hexadec number is :" + hexStr);
+			int decnum = Integer.parseInt(hexStr, 16);
+			String binary = Integer.toBinaryString(decnum);
+			System.out.println(binary); 
+ }		
+
+ }
