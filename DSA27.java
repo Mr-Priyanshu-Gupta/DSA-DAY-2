f1ca65d6f526632968111dd4806e0bd532ceb0fe
Mr-Priyanshu-Gupta <priyanshuguptaofficial555@gmail.com>
2025-07-14 18:56:08 +0530

    Add files via upload

---
+++ DSA27.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class DSA27
+{
+	public static void main(String args[])
+	{
+	  Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your binary number");
+ 		 int num1 = sc.nextInt();
+	 	System.out.println(" octal number is :" + num1);
+			int power = 1;
+			int result = 0 ;
+				while(num1 > 0 ){
+				int x = num1 % 10;
+				result = result + (x * power);
+				power = power * 8;
+				num1 = num1 / 10;
+			
+			}
+	 if(result == 0){
+		System.out.println("HEXAdec : 0");
+		}
+		while(result > 0){
+		 int HEXAdec = result % 16;
+		 result = result /16;
+		System.out.println("HEXAdecimal value : " + HEXAdec);
+  }
+
+			
+ }		
+
+ }
